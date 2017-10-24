dcd34dbeff807ae23b5e31da46936c7452f80b3c
Suikaba <suibaka@hotmail.co.jp>
2017-10-24 15:13:02 +0900

    implemented task3 agent

--- src/ch/idsia/agents/controllers/OwnAgent2.java
+++ src/ch/idsia/agents/controllers/OwnAgent2.java
@@ -28,8 +28,10 @@
 package ch.idsia.agents.controllers;
 
 import ch.idsia.agents.Agent;
+import ch.idsia.benchmark.mario.engine.GeneralizerLevelScene;
 import ch.idsia.benchmark.mario.engine.sprites.Mario;
 import ch.idsia.benchmark.mario.environments.Environment;
+import ch.idsia.benchmark.mario.engine.sprites.Sprite;
 import java.util.Random;
 
 /**
@@ -41,26 +43,143 @@ import java.util.Random;
 
 public class OwnAgent2 extends BasicMarioAIAgent implements Agent
 {
-int trueJumpCounter = 0;
-int trueSpeedCounter = 0;
+	final int MIN_R = 0;
+	final int MAX_R = 18;
+	final int MIN_C = 0;
+	final int MAX_C = 18;
 
-public OwnAgent2()
-{
-    super("OwnAgent");
-    reset();
-}
+	final int NO_OBSTACLE = 0;
 
-public void reset()
-{
-    action = new boolean[Environment.numberOfKeys];
-    action[Mario.KEY_RIGHT] = true;
-}
+	boolean good_jump = false;
+	int leftCounter = 0;
+	int rightCounter = 0;
 
-public boolean[] getAction()
-{
-    Random R = new Random();
-    action[Mario.KEY_DOWN] = R.nextBoolean();
+	public OwnAgent2()
+	{
+	    super("OwnAgent2");
+	    reset();
+	}
+
+	public boolean isObstacle(int r, int c) {
+		final int val = getReceptiveFieldCellValue(r, c);
+		return val == GeneralizerLevelScene.BRICK
+			   || val == GeneralizerLevelScene.BORDER_CANNOT_PASS_THROUGH
+			   || val == GeneralizerLevelScene.FLOWER_POT_OR_CANNON
+			   || val == GeneralizerLevelScene.LADDER;
+	}
 	
-    return action;
-}
+	public boolean isCreature(int r, int c) {
+		final int val = getEnemiesCellValue(r, c);
+		return val != Sprite.KIND_NONE;
+	}
+
+	public boolean isGaps(int c) {
+		for(int i = MIN_R; i <= MAX_R; ++i) {
+			if(getReceptiveFieldCellValue(i, c) != NO_OBSTACLE) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public void reset()
+	{
+		action = new boolean[Environment.numberOfKeys];
+	}
+	
+	public boolean canJump()
+	{
+		if(!isMarioAbleToJump) {
+			return false;
+		}
+		if(leftCounter >= 5) {
+			return true;
+		}
+		boolean ok = true;
+		for(int c = marioEgoCol; c <= marioEgoCol + 2; ++c) {
+			for(int r = marioEgoRow - 1; r >= 0; --r) {
+				ok &= !isCreature(r, c);
+			}
+		}
+		return ok;
+	}
+	
+	public int findFrontEnemy() {
+		int result = -1;
+		for(int c = marioEgoCol + 1; c <= MAX_C; ++c) {
+			if(isCreature(marioEgoRow, c)) {
+				result = c;
+				break;
+			}
+		}
+		return result;
+	}
+	
+	void goLeft() {
+		action[Mario.KEY_LEFT] = true;
+		action[Mario.KEY_RIGHT] = false;
+	}
+	void goRight() {
+		action[Mario.KEY_RIGHT] = true;
+		action[Mario.KEY_LEFT] = false;
+	}
+	void actionUpdate() {
+		if(action[Mario.KEY_LEFT]) {
+			leftCounter++;
+		} else {
+			leftCounter = 0;
+		}
+		if(action[Mario.KEY_RIGHT]) {
+			rightCounter++;
+		} else {
+			rightCounter = 0;
+		}
+	}
+
+	public boolean[] getAction()
+	{
+		final int NOW_R = marioEgoRow;
+		final int NOW_C = marioEgoCol;
+		
+		goRight();
+		action[Mario.KEY_SPEED] = true;
+		
+		boolean can_jump = isMarioAbleToJump || !isMarioOnGround;
+		if(isMarioAbleToJump) {
+			good_jump = false;
+		}
+		if(isObstacle(marioEgoRow, marioEgoCol + 1)) {
+			if(canJump()) {
+				action[Mario.KEY_JUMP] = can_jump;
+				goRight();
+			} else {
+				goLeft();
+			}
+		}
+		
+		int frontEnemyCol = findFrontEnemy();
+		if(frontEnemyCol != -1) {
+			action[Mario.KEY_SPEED] = false;
+			if(isMarioAbleToShoot) {
+				action[Mario.KEY_SPEED] = true;
+				goRight();
+			} else if(canJump()) {
+				action[Mario.KEY_JUMP] = true;
+				goRight();
+			} else {
+				goLeft();
+			}
+		}
+		if(isGaps(marioEgoCol + 1)) {
+			action[Mario.KEY_JUMP] = can_jump;
+			if(isMarioAbleToJump && isMarioOnGround && !isGaps(marioEgoCol)) {
+				good_jump = true;
+			}
+			if(!good_jump) {
+				goLeft();
+			}
+		}
+		actionUpdate();
+	    return action;
+	}
 }
